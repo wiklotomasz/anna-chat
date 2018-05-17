961c1410223268433851ee2cb7f22232ee8f96c6
Tomasz Wiklo <wikweb@gmail.com>
2018-05-17 15:38:06 +0200

    add files

---
+++ UsersService.js
@@ -0,0 +1,25 @@
+//serwis, który zajmie się zarządzaniem użytkownikami korzystającymi w danej chwili z czatu
+class UsersService {
+  constructor() {
+    this.users = [];
+  }
+
+  getAllUsers() {
+    return this.users;
+  }
+
+  getUserById(userId) {
+    return this.users.find(user => user.id === userId);
+  }
+
+  addUser(user) {
+    this.users = [user, ...this.users];
+  }
+
+  removeUser(userId) {
+    this.users = this.users.filter(user => user.id !== userId);
+  }
+}
+module.exports = UsersService;
+
+
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+var webpack = require('webpack');
+
+var env = process.env.NODE_ENV || 'development';
+var plugins = [
+  new HtmlWebpackPlugin({
+        template: 'client/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+      sourceMap: false
+    })
+  );
+}
+
+module.exports = {
+    entry: (env !== 'production' ? [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+    ] : []).concat(['./client/index.js']),
+
+    output: {
+      filename: './bundle.js',
+        path: path.resolve(__dirname, 'public'),
+        
+    },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+
+        ]
+      }
+    ]
+  },
+  plugins
+};
